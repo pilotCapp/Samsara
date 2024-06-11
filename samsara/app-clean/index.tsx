d095774945d859f1b7da8625092e510e5bee9cc1
kristoffer seyffarth <kristoffer.seyffarth@gmail.com>
2024-06-11 16:21:14 +0200

    build(init): :technologist: inital build and dependencies
    downloaded expo and required dependencies for ios and android simulation, also works using expo go

---
+++ samsara/app-clean/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Shake your phone to open the developer menu.</Text>
+    </View>
+  );
+}
