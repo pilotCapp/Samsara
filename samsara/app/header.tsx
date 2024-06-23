86d57c8800e65fd796262fa117adbbe802a08eaa
kristoffer seyffarth <kristoffer.seyffarth@gmail.com>
2024-06-23 20:45:40 +0200

    style(dev): :art: implement header logo styling

--- samsara/app/header.tsx
+++ samsara/app/header.tsx
@@ -3,16 +3,24 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { Service } from "@/types";
+import Svg, { Path } from "react-native-svg";
+import { Image, ImageTransition } from "expo-image";
 
 const Header = ({ service_data }: { service_data: Service }) => {
-	console.log(service_data.colors);
 	return (
 		<LinearGradient
 			// Button Linear Gradient
 			locations={[0, 0.95, 1]}
 			colors={service_data.colors}
 			style={styles.container}>
-			<Text style={styles.title}>{service_data.name}</Text>
+			<Image
+				source={service_data.image}
+				style={styles.logo}
+				transition={{
+					duration: 300, // Duration of the transition in milliseconds
+				}}
+				contentFit='contain'
+			/>
 		</LinearGradient>
 	);
 };
@@ -22,14 +30,14 @@ const styles = StyleSheet.create({
 		justifyContent: "flex-end",
 		alignItems: "center",
 		height: 150,
-		paddingHorizontal: 15,
+		paddingBottom:20,
 	},
 	title: {
 		color: "white",
 		fontSize: 40,
 		fontWeight: "bold",
-		marginBottom: 40,
 	},
+	logo: { height: "60%", width: "40%"},
 });
 
 export default Header;
