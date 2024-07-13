c28e2ffb666466fe8d88906baa7f21a0608328c9
kristoffer seyffarth <kristoffer.seyffarth@gmail.com>
2024-07-13 21:45:16 +0200

    feat(dev): :sparkles: date view while dragging dateline
    implements a calendar view for the dateline component while dragging, requires further styling

--- samsara/components/DateLine.tsx
+++ samsara/components/DateLine.tsx
@@ -19,6 +19,8 @@ const DateLine: React.FC<{
 	const dimensions = Dimensions.get("window");
 	const angleRef = useRef(0);
 	const selectedServicesRef = useRef(selected_services);
+	const dateViewOpacity = useRef(new Animated.Value(0)).current;
+	const [isDragging, setIsDragging] = useState(false);
 	const c = 1000 * 60 * 60 * 24;
 
 	const [linePositions, setLinePositions] = useState<number[]>(
@@ -40,7 +42,15 @@ const DateLine: React.FC<{
 	const panResponder = useRef(
 		PanResponder.create({
 			onStartShouldSetPanResponder: () => true,
-			onPanResponderGrant: () => {},
+			onPanResponderGrant: () => {
+				console.log(dateViewOpacity);
+				setIsDragging(true);
+				Animated.timing(dateViewOpacity, {
+					toValue: 1,
+					useNativeDriver: true,
+					duration: 300,
+				}).start();
+			},
 			onPanResponderMove: (event, gestureState) => {
 				const angle =
 					-Math.atan2(
@@ -58,6 +68,16 @@ const DateLine: React.FC<{
 			onPanResponderRelease: (_, gestureState) => {
 				const new_date = getDateFromAngle(angleRef.current);
 				periodUsestate[1](new_date);
+
+				Animated.timing(dateViewOpacity, {
+					toValue: 0,
+					useNativeDriver: true,
+					duration: 300,
+				}).start(() => setIsDragging(false));
+			},
+			onPanResponderTerminate: (_, gestureState) => {
+				setIsDragging(false);
+				dateViewOpacity.setValue(0);
 			},
 		})
 	).current;
@@ -86,8 +106,8 @@ const DateLine: React.FC<{
 			0,
 			Math.min(30, Math.round(timedifference))
 		);
-		const date=new Date(today.getTime() + daysDifference * c);
-		date.setHours(23, 59, 59, 999)
+		const date = new Date(today.getTime() + daysDifference * c);
+		date.setHours(23, 59, 59, 999);
 		return date;
 	}
 
@@ -100,7 +120,7 @@ const DateLine: React.FC<{
 
 	return (
 		<View style={styles.svg_container}>
-			<Animated.View
+			<View
 				{...panResponder.panHandlers}
 				style={{
 					position: "absolute",
@@ -109,13 +129,50 @@ const DateLine: React.FC<{
 					top: linePositions[1] - 40,
 					left: linePositions[0] - 40,
 					borderRadius: 100,
-					transform: [{ rotate: `${angleRef.current}rad` }],
 				}}>
 				<Image
 					source={require("../assets/images/watchhand.png")}
-					style={{ width: "100%", height: "100%" }}
+					style={{
+						width: "100%",
+						height: "100%",
+						transform: [{ rotate: `${angleRef.current}rad` }],
+					}}
 					resizeMode={"contain"}></Image>
-			</Animated.View>
+				{isDragging ? (
+					<Animated.View
+						style={{
+							position: "absolute",
+							opacity: dateViewOpacity,
+							height: 100,
+							width: 100,
+							top: -80,
+							left: 80,
+						}}>
+						<Image
+							source={require("../assets/images/dateline_calendar.png")}
+							resizeMode={"contain"}
+							style={{ width: "100%", height: "100%" }}
+						/>
+						<View
+							style={{
+								position: "absolute",
+								flex: 1,
+								alignItems: "center",
+								justifyContent: "center",
+								height: "100%",
+								width: "100%",
+							}}>
+							<Animated.Text style={{ color: "white", padding: 10 }}>
+								{getDateFromAngle(angleRef.current).toLocaleString("default", {
+									weekday: "long",
+									day: "2-digit",
+									month: "long",
+								})}
+							</Animated.Text>
+						</View>
+					</Animated.View>
+				) : null}
+			</View>
 		</View>
 	);
 };
